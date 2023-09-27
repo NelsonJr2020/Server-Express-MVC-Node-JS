542cb45020f1beaa3ccf517e17b93d17f1504e64
Nelson Alderete <62829278+NelsonJr2020@users.noreply.github.com>
2023-09-26 23:56:40 -0300

    Update auth.middleware
    Se actualiza el middleware encargado de supervisar las rutas protegidas chekeando primero la sesión y luego la autenticación JWT (Json Web Token)

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,71 +1,85 @@
 /* middlewares/auth.middleware.js */
 
 //REQUIRES
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const messageService = require("../services/message.service");
 
-//CLASE AUTHMIDDLEWARE DE SEGURIDAD Y ACCESO
+//CLASE AUTHMIDDLEWARE DE SEGURIDAD Y ACCESO UTILIZANDO JWT
 class AuthMiddleware {
+  #secretKey = process.env.JWT_SECRET;
+  #apiKey = process.env.API_KEY;
 
-    #secretKey = process.env.JWT_SECRET || 'eyJhbGciOiJIUzUxMiJ9.eyJSb2xlIjoiQWRtaW5TdXBlclVzZXJOb25lU21hc2hUaGUuLi4';
-    #apiKey = process.env.API_KEY || 'e7c5ee2c-0a90-459b-88f1-1bb215b020b4';
-     
-    //COMPROBACIÓN DE VALIDEZ DE SESIÓN DE USUARIO
-    authenticateSession(req, res, next) {
-      if(req.session.userId) {
-        res.locals.isAuthenticated = true;
-        next();
-      } else {
-        res.locals.isAuthenticated = false;
-        req.session.message = "Se requiere iniciar sesión para ver su perfil";
-        res.redirect('/');
-      }
+  //COMPROBACIÓN DE VALIDEZ DE SESIÓN DE USUARIO
+  authenticateSession(req, res, next) {
+    if (req.session.userId) {
+      res.locals.isAuthenticated = true;
+      next();
+    } else {
+      res.locals.isAuthenticated = false;
+      req.session.message = "Inicie sesión para acceder al sitio";
+      res.redirect("/");
     }
+  }
 
-    //COMPROBACIÓN DE VALIDEZ DE JSON WEB TOKEN DE USUARIO
-    authenticateJWT = (req, res, next) => {
-        const token = req.cookies.jwt;
-        if (token) {
-            try {
-              const decoded = this._verifyToken(token);
-              if(decoded) {
-                req.user = decoded;
-                next();
-              } else {
-                res.status(401).json({ message: "Acceso no autorizado" }).redirect('/');
-              }
-            } catch (error) {
-              res.status(401).json({ message: "Token inválido o expirado" }).redirect('/');
-            }
-        } else {
-          res.status(401).json({ message: "Acceso no autorizado" }).redirect('/');
-        }
-    }
-
-    //COMPROBACIÓN DE VALIDEZ DE APIKEY DE USUARIO
-    authenticateAPIKey = (req, res, next) => {
-        const apiKey = req.headers["x-api-key"];
-    
-        if (apiKey === this.#apiKey) {
-          next();
-        } else {
-          res.status(401).json({ message: "Acceso no autorizado" });
-        }
-    }
-
-    //GENERACIÓN DE TOKEN JWT
-    generateToken = (payload, time) => { // El token expira en el parámetro time
-      return jwt.sign(payload, this.#secretKey, { expiresIn: time }); // Ejemplo '1h' (una hora)
-    }
-
-    //VERIFICACIÓN DE TOKEN JWT
-    _verifyToken = (token) => {
+  //COMPROBACIÓN DE VALIDEZ DE JSON WEB TOKEN DE USUARIO
+  authenticateJWT = (req, res, next) => {
+    const token = req.cookies.jwt;
+    const error = [];
+    if (token) {
       try {
-        return jwt.verify(token, this.#secretKey);
+        const decoded = this._verifyToken(token);
+        if (decoded) {
+          req.user = decoded;
+          return next();
+        } else {
+          messageService.addMessage("Acceso no autorizado", "error");
+          res.status(401).redirect("/");
+        }
       } catch (error) {
-        throw new Error('Token inválido o expirado');
+        messageService.addMessage("Su token es inválido", "error");
+        res.status(401).redirect("/");
       }
+    } else {
+      res.locals.isAuthenticated = false;
+      res.status(401).redirect("/");
     }
+  };
 
+  /* COMPROBACIÓN DE VALIDEZ DE APIKEY DE USUARIO 
+     ESTA SECCIÓN ESTÁ PENSADA PARA EXTENDER ALGUNA
+     FUNCIONALIDAD EXTRA CON OTRA APP...
+  authenticateAPIKey = (req, res, next) => {
+    const apiKey = req.headers["x-api-key"];
+
+    if (apiKey === this.#apiKey) {
+      return next();
+    } else {
+      messageService.addMessage("Acceso no autorizado", "error");
+      error.status = 401;
+      return next(error);
+    }
+  };
+*/
+
+  //GENERACIÓN DE TOKEN JWT
+  generateToken = (payload, time) => {
+    // El token expira en el parámetro time
+    try {
+      return jwt.sign(payload, this.#secretKey, { expiresIn: time }); // Ejemplo '1h' (una hora)
+    } catch (error) {
+      throw new Error("Error al generar el token");
+    }
+  };
+
+  //VERIFICACIÓN DE TOKEN JWT
+  _verifyToken = (token) => {
+    try {
+      return jwt.verify(token, this.#secretKey);
+    } catch (error) {
+      throw new Error("Su token no es válido");
+    }
+  };
 }
-  
+
+//EXPORTA LA INSTANCIA DE LA CLASE MIDDLEWARE DE AUTENTICACIONES
 module.exports = new AuthMiddleware();
