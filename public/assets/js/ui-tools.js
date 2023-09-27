3af3b74934bc9475f422ad927c2d80bb042e4ebd
Nelson Alderete <62829278+NelsonJr2020@users.noreply.github.com>
2023-09-26 23:13:10 -0300

    Update and rename tools.js to ui-tools.js
    Se actualiza este archivo según los nuevos requerimientos de las vistas y para optimizar el funcionamiento de cada vista con menos código

---
+++ public/assets/js/ui-tools.js
@@ -0,0 +1,28 @@
+/* USER INTERFACE TOOL JS */
+
+let tooltipTriggerList;
+let tooltipList;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-toggle="collapse"]');
+
+    tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', function () {
+            const targetId = this.getAttribute('data-target');
+            const targetElement = document.querySelector(targetId);
+            
+            if (targetElement) {
+                if (targetElement.classList.contains('show')) {
+                    targetElement.classList.remove('show');
+                } else {
+                    targetElement.classList.add('show');
+                }
+            }
+        });
+    });
+});
